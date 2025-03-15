94450cd68960f07c1bcd0fa44ca0ae2d20795408
CatsheeRS <184840989+CatsheeRS@users.noreply.github.com>
2025-03-15 23:34:11 +0000

    starry.net. now with seizures!

--- game/Species/Player.cs
+++ game/Species/Player.cs
@@ -1,4 +1,5 @@
-﻿using Starry.NET.Objects;
+﻿using Raylib_cs;
+using Starry.NET.Objects;
 using Starry.NET.Objects.Assets;
 using Starry.NET.Utils;
 using System;
@@ -12,18 +13,29 @@ namespace Game.Species
 {
     public class Player : Entity
     {
+        private Sprite playerSprite;
+        private int time;
+        private Random rand = new();
+
         public override void Create()
         {
-            Sprite sprite = new Sprite();
-            sprite.Image = (StImage)Assets.Load<StImage>("player.png");
+            playerSprite = new Sprite();
+            playerSprite.Image = (StImage)Assets.Load<StImage>("player.png");
 
             Position = new Vector2(100, 100);
-            AddComponent(sprite);
+            AddComponent(playerSprite);
             Log("Player has been created");
         }
 
         public override void Update()
         {
+            time++;
+            if (time % 60 == 0)
+            {
+                Log("Updated player colour");
+                playerSprite.Colour = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255), 255);
+                time = 0;
+            }
         }
     }
 }
